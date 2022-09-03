34679e60d08126507503d12b7452f2d55aa41cae
darleydias <darlley@gmail.com>
2022-09-02 21:43:36 -0300

    2.4.1

--- models/operacoes.js
+++ models/operacoes.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose')
 var Schema = mongoose.Schema;
-var comarcaSchema = new Schema({
+var operacaoSchema = new Schema({
     codigo:{type:String,require:true,unique:true},
     nome:{type:String,require:true},
     descricao:{type:String},
     dataCriacao:{type:Date,default:Date.now()},
 })
 
-module.exports = mongoose.model('Operacoes',comarcaSchema)
+module.exports = mongoose.model('Operacoes',operacaoSchema)
